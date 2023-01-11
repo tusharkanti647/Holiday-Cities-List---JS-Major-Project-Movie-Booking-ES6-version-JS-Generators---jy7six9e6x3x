5bc46fd7b82df6eebf61f8fb5bc69fd3a4ca5bb6
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-11 18:22:22 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+/*
 class App extends Component {
   constructor(props) {
     super(props)
@@ -40,11 +41,67 @@ class App extends Component {
   render() {
     return (
       <div id="main">
-        {/* Do not remove the main div */}
+         //{ Do not remove the main div }
       </div>
     )
   }
 }
 
+
+export default App;
+*/
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.cityList = [
+      { name: "Goa", country: "India" },
+      { name: "Amsterdam", country: "Netherlands" },
+      { name: "New York", country: "USA" },
+      { name: "Darjeeling", country: "India" },
+      { name: "Tokyo", country: "Japan" },
+      { name: "Lonavala", country: "India" },
+      { name: "Brandenburg Gate", country: "Germany" },
+      { name: "Reichstag Building", country: "Germany" },
+      { name: "Museum Island", country: "Germany" },
+      { name: "Munnar", country: "India" },
+      { name: "Leh Ladakh", country: "India" },
+      { name: "Goa", country: "India" },
+      { name: "Agra", country: "India" },
+      { name: "Dalhousie", country: "India" },
+      { name: "Coorg", country: "India" },
+      { name: "Rome", country: "Italy" },
+      { name: "Milan", country: "Italy" },
+      { name: "Venice", country: "Italy" },
+      { name: "Varanasai", country: "India" },
+      { name: "Jaipur", country: "India" },
+      { name: "The Hofburg", country: "Austria" },
+      { name: "Belvedere Palace", country: "Austria" },
+      { name: "St. Stephen Cathedral", country: "Austria" },
+      { name: "Kahna National Park", country: "India" },
+      { name: "Amritsar", country: "India" },
+      { name: "Mussoorie", country: "India" },
+      { name: "Mount Abu", country: "India" },
+      { name: "Tirupati", country: "India" },
+    ];
+    this.valid = this.valid.bind(this);
+  }
+
+  valid() {
+    let a = [];
+    let j = 1;
+    for (let i = 0; i < this.cityList.length; i++) {
+      if (this.cityList[i].country === "India") {
+        a.push(<li key={"location" + j++}>{this.cityList[i].name}</li>);
+      }
+    }
+    return a;
+  }
+
+  render() {
+    return <ol>{this.valid()}</ol>;
+  }
+}
 
 export default App;
